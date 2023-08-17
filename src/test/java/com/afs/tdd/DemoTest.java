7257c84971b2bb6d63bcd6d95666c7bf0ac44bdb
Guile <jimenezguiles7255@gmail.com>
2023-08-18 01:46:48 +0800

    TEST: create a test for should_return_to_location_0_1_N_when_executeCommand_given_0_0_North_and_command_move

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,22 @@
 package com.afs.tdd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_to_location_0_1_N_when_executeCommand_given_0_0_North_and_command_move() {
+        Location initialLocation = new Location(0, 0,Direction.NORTH );
+        Command givenCommand = Command.MOVE;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(0, currentLocation.getX());
+        Assertions.assertEquals(1, currentLocation.getY());
+        Assertions.assertEquals(Direction.NORTH, currentLocation.getDirection());
+
 
     }
 }
