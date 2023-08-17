0884ddd7f77d5808d5117e0ec23bc5a1c2158b17
Guile <jimenezguiles7255@gmail.com>
2023-08-18 01:55:27 +0800

    TEST: Create a test for move when the given direction is EAST or SOUTH or WEST

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -10,7 +10,7 @@ class DemoTest {
         Command givenCommand = Command.MOVE;
         MarsRover marsRover = new MarsRover(initialLocation);
 
-        marsRover.executeCommand(givenCommand);
+        marsRover.executeMoveCommand(givenCommand);
         Location currentLocation = marsRover.getCurrentLocation();
 
         Assertions.assertEquals(0, currentLocation.getX());
@@ -18,5 +18,46 @@ class DemoTest {
         Assertions.assertEquals(Direction.NORTH, currentLocation.getDirection());
 
 
+    }
+
+    @Test
+    void should_return_to_location_1_0_E_when_executeCommand_given_0_0_East_and_command_move(){
+        Location initialLocation = new Location(0, 0,Direction.EAST );
+        Command givenCommand = Command.MOVE;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeMoveCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(1, currentLocation.getX());
+        Assertions.assertEquals(0, currentLocation.getY());
+        Assertions.assertEquals(Direction.EAST, currentLocation.getDirection());
+    }
+
+    @Test
+    void should_return_to_location_negative_1_0_S_when_executeCommand_given_0_0_South_and_command_move(){
+        Location initialLocation = new Location(0, 0,Direction.SOUTH );
+        Command givenCommand = Command.MOVE;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeMoveCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(0, currentLocation.getX());
+        Assertions.assertEquals(-1, currentLocation.getY());
+        Assertions.assertEquals(Direction.SOUTH, currentLocation.getDirection());
+    }
+    @Test
+    void should_return_to_location_0_negative_1_S_when_executeCommand_given_0_0_West_and_command_move(){
+        Location initialLocation = new Location(0, 0,Direction.WEST );
+        Command givenCommand = Command.MOVE;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeMoveCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(-1, currentLocation.getX());
+        Assertions.assertEquals(0, currentLocation.getY());
+        Assertions.assertEquals(Direction.WEST, currentLocation.getDirection());
     }
 }
