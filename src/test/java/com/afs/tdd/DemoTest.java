177c8fbb1b43fc25bcdd324278f46b432973dfed
Guile <jimenezguiles7255@gmail.com>
2023-08-18 02:20:24 +0800

    TEST: Create a test the will return the specific direction when turn right command

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -67,7 +67,46 @@ class DemoTest {
         Command givenCommand = Command.RIGHT;
         MarsRover marsRover = new MarsRover(initialLocation);
 
-        marsRover.executeTurnRightCommand(givenCommand);
+        marsRover.executeRightCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(0, currentLocation.getX());
+        Assertions.assertEquals(0, currentLocation.getY());
+        Assertions.assertEquals(Direction.EAST, currentLocation.getDirection());
+    }
+    @Test
+    void should_return_to_location_0_0_S_when_executeTurnRightCommand_given_0_0_East_and_command_turn_right(){
+        Location initialLocation = new Location(0, 0,Direction.NORTH );
+        Command givenCommand = Command.RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeRightCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(0, currentLocation.getX());
+        Assertions.assertEquals(0, currentLocation.getY());
+        Assertions.assertEquals(Direction.EAST, currentLocation.getDirection());
+    }
+    @Test
+    void should_return_to_location_0_0_W_when_executeTurnRightCommand_given_0_0_South_and_command_turn_right(){
+        Location initialLocation = new Location(0, 0,Direction.NORTH );
+        Command givenCommand = Command.RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeRightCommand(givenCommand);
+        Location currentLocation = marsRover.getCurrentLocation();
+
+        Assertions.assertEquals(0, currentLocation.getX());
+        Assertions.assertEquals(0, currentLocation.getY());
+        Assertions.assertEquals(Direction.EAST, currentLocation.getDirection());
+    }
+    @Test
+    void should_return_to_location_0_0_N_when_executeTurnRightCommand_given_0_0_West_and_command_turn_right(){
+        Location initialLocation = new Location(0, 0,Direction.NORTH );
+        Command givenCommand = Command.RIGHT;
+        MarsRover marsRover = new MarsRover(initialLocation);
+
+        marsRover.executeRightCommand(givenCommand);
         Location currentLocation = marsRover.getCurrentLocation();
 
         Assertions.assertEquals(0, currentLocation.getX());
